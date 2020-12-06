5ea5fefee72c7a63667c21c9372ae9ace6b20eac
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2020-12-07 01:22:37 +0200

    added a navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import Product from './Product/Product'
+import useStyles from './styles'
+
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price:'$5', image: 'https://thumblr.uniid.it/product/194739/7821e8f021ac.jpg'},
@@ -9,8 +11,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
