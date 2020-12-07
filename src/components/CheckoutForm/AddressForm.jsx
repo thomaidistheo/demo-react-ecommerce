f630faf3d3d6d00c9b3364effde6ca856c921383
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2020-12-08 01:21:45 +0200

    added order review

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -7,7 +7,7 @@ import { commerce } from '../../lib/commerce'
 
 import FormInput from './CustomTextField'
 
-const AddressForm = ({ checkoutToken }) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const [shippingCountries, setShippingCountries] = useState([])
     const [shippingCountry, setShippingCountry] = useState('')
     const [shippingSubdivisions, setShippingSubdivisions] = useState([])
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -48,7 +48,7 @@ const Checkout = ({ cart }) => {
 
     const Form = () => activeStep === 0 
         ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-        : <PaymentForm />
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken}/>
 
     return (
         <>
