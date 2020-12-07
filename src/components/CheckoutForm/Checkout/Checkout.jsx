866b4ed11ff42a9abd805533b47c511c54fc953c
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2020-12-08 00:43:34 +0200

    finalized shipping form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -12,6 +12,7 @@ const steps = ['Shipping Address', 'Payment Details']
 const Checkout = ({ cart }) => {
     const [activeStep, setActiveStep] = useState(0)
     const [checkoutToken, setCheckoutToken] = useState(null)
+    const [shippingData, setShippingData] = useState({})
     const classes = useStyles()
 
     useEffect(() => {
@@ -30,6 +31,15 @@ const Checkout = ({ cart }) => {
         generateToken()
     }, [])
 
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1 )
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1 )
+
+    const next = (data) => {
+        setShippingData(data);
+
+        nextStep()
+    }
+
     const Confirmation = () => (
         <div>
             Confirmation
@@ -37,7 +47,7 @@ const Checkout = ({ cart }) => {
     )
 
     const Form = () => activeStep === 0 
-        ? <AddressForm checkoutToken={checkoutToken} />
+        ? <AddressForm checkoutToken={checkoutToken} next={next}/>
         : <PaymentForm />
 
     return (
